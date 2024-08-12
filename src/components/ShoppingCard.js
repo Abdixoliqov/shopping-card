ba666af3504e19d13bb12d066da017d0d9e59a0c
Abdixoliqov <abdixoliqvhamidullo@gmail.com>
2024-08-12 12:47:30 +0500

    abc

--- src/components/ShoppingCard.js
+++ src/components/ShoppingCard.js
@@ -1,15 +1,24 @@
 import ProductCard from "./ProductCard";
+import ShoppingCardItem from "./ShoppingCardItem";
 
 
 function ShoppingCard() {
     return ( 
         <div className="shopping-card">
             <div className="product">
+                <h3>Product</h3>
                 <div className="product-containr">
                     <ProductCard product={{id: 1, name:'Qazi pitsa', image: 'https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2Fbd276106-b16a-4c07-a963-1ccffef5e30d.jpg&w=1920&q=100', price: 3}}/>
                     <ProductCard product={{id: 2, name:'Qazi pitsa', image: 'https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2Fbd276106-b16a-4c07-a963-1ccffef5e30d.jpg&w=1920&q=100', price: 4.5}}/>
                     <ProductCard product={{id: 3, name:'Qazi pitsa', image: 'https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2Fbd276106-b16a-4c07-a963-1ccffef5e30d.jpg&w=1920&q=100', price: 5}}/>
                     <ProductCard product={{id: 4, name:'Qazi pitsa', image: 'https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2Fbd276106-b16a-4c07-a963-1ccffef5e30d.jpg&w=1920&q=100', price: 6}}/>
+                </div>
+            </div>
+
+            <div className="card">
+                <h2>Card</h2>
+                <div className="card-item">
+                    <ShoppingCardItem item={{id: 1, name:'Qazi pitsa', image: 'https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2Fbd276106-b16a-4c07-a963-1ccffef5e30d.jpg&w=1920&q=100', price: 3}} />
                 </div>
             </div>
         </div>
--- src/components/ShoppingCardItem.js
+++ src/components/ShoppingCardItem.js
@@ -1,11 +1,19 @@
 
 
-function ShoppingCardItem({items}) {
-    return ( 
-        <div>
+function ShoppingCardItem({ item }) {
+    return (
+        <div className="product-container">
 
+            <div className="product-card">
+                <img src={item.image} alt={item.name} />
+                <div className="product-details">
+                    <h3>{item.name}</h3>
+                    <p>Price: {item.price}$</p>
+                   
+                </div>
+            </div>
         </div>
-     );
+    );
 }
 
 export default ShoppingCardItem;
